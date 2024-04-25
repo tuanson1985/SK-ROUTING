d5fcd7eab4802f2aa88cb2b9e7199795cc5c34fe
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-25 10:45:01 +0700

    Route Navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
 <h1>Welcome home!</h1>
+
+<a href="/blog">Blog</a>
+<a href="/products">Products</a>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,4 +1,10 @@
-<h1>Product List</h1>
-<h2>Product 1</h2>
-<h2>Product 2</h2>
-<h2>Product 3</h2>
+<script>
+    export let productId = 100;
+</script>
+
+<a href="/">Home</a>
+
+<h1><a href="/products">Product List</a></h1>
+<h2><a href="/products/1">Product 1</a></h2>
+<h2><a href="/products/2">Product 2</a></h2>
+<h2><a href={`/products/${productId}`}>Product {productId}</a></h2>

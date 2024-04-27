9191a29408107e19e9d5791df1b055f3080c2f8d
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-27 09:03:38 +0700

    Route Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<header class="layout-header">Header</header>
+<slot />
+<footer class="layout-footer">Footer</footer>
+
+<style>
+    .layout-header{
+        background-color: #264653;
+        color: #f4a261;
+        font-size: 30px;
+        text-align: center;
+        padding: 10px;
+    }
+    .layout-footer{
+        background-color: #264653;
+        color: #e9c46a;
+        font-size: 30px;
+        text-align: center;
+        padding: 10px;
+    }
+</style>

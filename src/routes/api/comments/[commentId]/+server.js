424a0325293682458399b7aac0ec06f318a3d308
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-27 14:25:19 +0700

    API Patch Request

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -6,6 +6,15 @@ export function GET(requestEvent) {
     const { commentId } = params;
     const comment = comments.find(
         (comment) => comment.id === parseInt(commentId)
-    )
+    );
+    return json(comment);
+}
+
+export async function PATCH(requestEvent) {
+    const { params, request } = requestEvent;
+    const { commentId } = params;
+    const { text } = await request.json();
+    const comment = comments.find((comment) => comment.id === parseInt(commentId));
+    comment.text = text;
     return json(comment);
 }

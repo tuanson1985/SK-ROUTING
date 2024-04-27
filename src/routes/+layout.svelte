a5e9757100de17a5d90e7c375a1498478f7ba1be
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-27 10:15:27 +0700

    Other Files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,7 @@
-<header class="layout-header">Header</header>
+<script>
+    import Header from "$lib/header.svelte";
+    import Footer from "$lib/footer.svelte";
+</script>
+<Header />
 <slot />
-<footer class="layout-footer">Footer</footer>
-
-<style>
-    .layout-header{
-        background-color: #264653;
-        color: #f4a261;
-        font-size: 30px;
-        text-align: center;
-        padding: 10px;
-    }
-    .layout-footer{
-        background-color: #264653;
-        color: #e9c46a;
-        font-size: 30px;
-        text-align: center;
-        padding: 10px;
-    }
-</style>
+<Footer/>

af636f3864ea1373f84668add9f97ad9b6e3f62b
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-27 08:48:45 +0700

    Route Matchers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,20 @@
+<script>
+    import { goto, beforeNavigate, afterNavigate } from '$app/navigation'
+    const handleClick = () => {
+        console.log('Placing your');
+        goto('/products',{replaceState: true})
+    }
+    beforeNavigate((navigation) => {
+        console.log({before: navigation})
+    })
+    afterNavigate((navigation) => {
+        console.log({after: navigation})
+    })
+</script>
+
 <h1>Welcome home!</h1>
 
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
+
+<button on:click={handleClick}>Palace order</button>
